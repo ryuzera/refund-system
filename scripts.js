39230a61668b4d3fb3743c6c1efc98c783258922
ryuzera <ryugfx2@gmail.com>
2024-11-19 16:15:34 -0300

    Criando um objeto de nova despesa

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -22,4 +25,20 @@ function formatCurrencyBRL(value) {
 
     // Retorna o valor formatado
     return value
+}
+
+// Captura o evento de submit do formulário para obter os valores
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes na nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        create_at: new Date()
+    }
 }
